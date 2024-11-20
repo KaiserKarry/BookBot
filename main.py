dc6f8037acbdf601a5ff5726e91aa9b89d6822c2
KaiserKarry <ninjabrosp@gmail.com>
2024-11-20 02:53:21 -0500

    Updated main.py with get_book_text function and updated main to be cleaner

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 def main():
-    with open("books/frankenstein.txt") as f:
-        file_contents = f.read()
-        print(file_contents)
+    book_path = "books/frankenstein.txt"
+    book_text = get_book_text(book_path)
+    print(book_text)
+
+def get_book_text(book_path):
+    with open(book_path) as f:
+        return f.read()
 
 main()
