7b7bbbe3c0516435b08f5f3a5d02b57541e746a8
KaiserKarry <ninjabrosp@gmail.com>
2024-11-21 03:03:22 -0500

    updated main.py to print a count of each character in a text

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ def main():
     book_path = "books/frankenstein.txt"
     book_text = get_book_text(book_path)
     word_count = number_of_words(book_text)
-    print(word_count)
+    letter_count = number_of_letters(book_text)
+    print(letter_count)
 
 def get_book_text(book_path):
     with open(book_path) as f:
@@ -11,5 +12,20 @@ def get_book_text(book_path):
 def number_of_words(text):
     words = text.split()
     return len(words)
+
+def number_of_letters(text):
+    # convert text to all lowercase
+    lowered_text = text.lower()
+
+    # make empty dictionary to populate
+    letters = {}
+
+    # iterate over each character checking if its in the dictionary or not. If it is then raise the count by 1, if it isnt then add an entry.
+    for character in lowered_text:
+        if character in letters:
+            letters[character] += 1
+        else:
+            letters[character] = 1
+    return letters
 
 main()
