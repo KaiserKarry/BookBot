b13cba55b90613bd6aec3d19856aa1f81712db46
KaiserKarry <ninjabrosp@gmail.com>
2024-11-22 01:35:25 -0500

    Fixed seperate_letter_dictionaries function to make a list of dictionaries. Added sort function and print_loop to print out final report.

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ def main():
     book_text = get_book_text(book_path)
     word_count = number_of_words(book_text)
     letter_count = number_of_letters(book_text)
-    seperate_letter_dictionaries(letter_count)
-    # print(letter_count)
+    letter_dictionaries_list = seperate_letter_dictionaries(letter_count)
+    letter_dictionaries_list.sort(reverse=True, key=sort_on)
+    print_loop(letter_dictionaries_list, word_count)
 
 def get_book_text(book_path):
     with open(book_path) as f:
@@ -30,17 +31,27 @@ def number_of_letters(text):
     return letters
 
 def seperate_letter_dictionaries(text):
-    # empty list
     seperated_list = []
 
-    # iterating over every entry in the text dictionary and adding it seperately to the list
+    # iterating over every entry in the text dictionary and adding it seperately to the list if it is a alpha.
     for i in text:
         if i.isalpha():
-            seperated_list += i
-            print (f"key is {i}, value is {text[i]}")
+            individual_entry = {}
+            individual_entry["letter"] = i 
+            individual_entry["number"] = text[i]
+            seperated_list.append(individual_entry)
         else:
             pass
     return seperated_list
 
+def sort_on(dict):
+    return dict["number"]
 
+# function to print the desired stats for frankenstein. Havent learnt about user input yet.
+def print_loop(dict, word_count):
+    print("------- Begin Report --------")
+    print(f"{word_count} words were found in the book of Frankenstein")
+    for i in dict:
+        print(f"The '{i["letter"]}' character was found {i["number"]} times.")
+    print("--- End Report ---")
 main()
